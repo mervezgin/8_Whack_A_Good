92fcfd5d9b7b72412c4ad9cb8b95c8688c8b47f5
mervezgin <mervezgin@gmail.com>
2024-04-22 11:23:41 +0300

    clean up code

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,18 +6,40 @@ public class Target : MonoBehaviour
 {
     Rigidbody targetRb;
 
+    float minSpeed = 12.0f;
+    float maxSpeed = 16.0f;
+    float maxTorque = 10.0f;
+    float xRange = 4.0f;
+    float ySpawnPos = -6.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
-        transform.position = new Vector3(Random.Range(-4, 4), -6, 0);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos, 0);
     }
 }
